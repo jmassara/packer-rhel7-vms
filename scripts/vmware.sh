6b9f76e9646a8c2e90154b0b42210191ae9999c1
James Massara <james_massara@intuit.com>
2015-04-16 09:42:41 -0700

    Removed HGFS patching, fixed in newer release of vmware

--- scripts/vmware.sh
+++ scripts/vmware.sh
@@ -10,18 +10,6 @@ mount -o loop $VMWARE_ISO $VMWARE_MNTDIR
 tar zxf $VMWARE_MNTDIR/VMwareTools*.tar.gz -C $VMWARE_TMPDIR
 umount $VMWARE_MNTDIR
 
-# Patch HGFS
-curl -Lo $VMWARE_TMPDIR/hgfs_patch.tar.gz https://gist.github.com/jmassara/e794a6ed80543d7c75fb/download
-tar zxf $VMWARE_TMPDIR/hgfs_patch.tar.gz -C $VMWARE_TMPDIR
-cd $VMWARE_TMPDIR/vmware-tools-distrib/lib/modules/source/
-tar xf vmhgfs.tar
-cd vmhgfs-only
-patch inode.c < $VMWARE_TMPDIR/gist*/vmhgfs-d_count-kernel-3.10-tools-9.6.2.patch
-cd ..
-rm -f vmhgfs.tar
-tar cf vmhgfs.tar vmhgfs-only
-cd ~
-
 # Install tools
 $VMWARE_TMPDIR/vmware-tools-distrib/vmware-install.pl -d
 
